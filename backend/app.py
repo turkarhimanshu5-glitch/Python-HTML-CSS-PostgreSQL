044716d4d7dffbac6802c331aaa3ca1bd22cc8cc
turkarhimanshu5-glitch <turkarhimanshu5@gmail.com>
2026-07-01 15:46:59 +0530

    Refactor Flask app and add home and API routes
    Refactored the Flask app structure by removing the create_app function and adding new routes for home and API endpoints.

--- backend/app.py
+++ backend/app.py
@@ -1,41 +1,21 @@
-import os
-from flask import Flask
-from database.db import db
-from flask_cors import CORS
-from dotenv import load_dotenv
-from utils.errors import BadRequestException
-from blueprints.users import users_blueprint
-from utils.http import bad_request, not_found, not_allowed, internal_error
+from flask import Flask, jsonify
 
-load_dotenv()  # load env files
+app = Flask(__name__)
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(os.getenv('APP_SETTINGS'))
-    app.url_map.strict_slashes = False
-    db.init_app(app)
-    CORS(app)
-
-    app.register_blueprint(users_blueprint, url_prefix='/api/v1')
-
-    @app.errorhandler(BadRequestException)
-    def bad_request_exception(e):
-        return bad_request(e)
-
-    @app.errorhandler(404)
-    def route_not_found(e):
-        return not_found('route')
-
-    @app.errorhandler(405)
-    def method_not_allowed(e):
-        return not_allowed()
-
-    @app.errorhandler(Exception)
-    def internal_server_error(e):
-        return internal_error()
-
-    return app
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Flask Backend is Running Successfully",
+        "status": "OK"
+    })
 
 
-app = create_app()
+@app.route("/api")
+def api():
+    return jsonify({
+        "message": "Hello from Python Backend API"
+    })
+
+
+# Do not use app.run() in online runners
